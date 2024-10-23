abbccf4c5d841825ae8b79dfe53d8ccded57c48f
Irara <irara.chen@token.im>
2024-10-23 13:02:07 +0800

    feat: Add postOpRevertReason event count

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import axios from "axios";
+import { keccak256, toUtf8Bytes, JsonRpcProvider } from "ethers";
+import "./App.css";
+
+const rpcUrl = import.meta.env.VITE_RPC_URL;
+const etherScanApiUrl = import.meta.env.VITE_ETHER_SCAN_API_URL;
+const etherScanApiKey = import.meta.env.VITE_ETHER_SCAN_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState<any>();
+
+  // Transaction must have been sent to EntryPoint contract
+  const entryPointAddress = "0x0000000071727De22E5E9d8BAf0edAc6f37da032";
+
+  // Identifier for PostOpRevertReason event
+  // = "0xf62676f440ff169a3a9afdbf812e89e7f95975ee8e5c31214ffdef631c5f4792"
+  const postOpRevertReasonId = keccak256(
+    toUtf8Bytes("PostOpRevertReason(bytes32,address,uint256,bytes)")
+  );
+
+  const calculateLogNumberFromApi = async () => {
+    const FROM_BLOCK = "";
+    const TO_BLOCK = "";
+    const PAGE = "";
+    const OFFSET = "";
+
+    // Refer: https://docs.etherscan.io/api-endpoints/logs#get-event-logs-by-topics
+    const request = `https://${etherScanApiUrl}?module=logs
+    &action=getLogs
+    &address=${entryPointAddress}
+    &fromBlock=${FROM_BLOCK}
+    &toBlock=${TO_BLOCK}
+    &topic0=${postOpRevertReasonId}
+    &page=${PAGE}
+    &offset=${OFFSET}
+    &apikey=${etherScanApiKey}`.replace(/\s+/g, "");
+
+    const response = await axios.get(request);
+
+    setData(response.data.result);
+
+    const message = response.data.message;
+    const count = response.data.result.length;
+
+    console.log(`message: ${message}, count: ${count}`);
+  };
+
+  const calculateLogNumberFromEthers = async () => {
+    const provider = new JsonRpcProvider(rpcUrl);
+    const eventFilter = {
+      address: entryPointAddress,
+      topics: [postOpRevertReasonId],
+      fromBlock: 0,
+    };
+    const logs = await provider.getLogs(eventFilter);
+    setData(logs);
+    console.log(`count: ${logs.length}`);
+  };
+
+  const printData = () => {
+    console.log(`data: ${JSON.stringify(data, null, 2)}`);
+  };
 
   return (
     <>
@@ -25,11 +84,22 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div className="card">
+        <button onClick={calculateLogNumberFromApi}>From etherscan API</button>
+      </div>
+      <div className="card">
+        <button onClick={calculateLogNumberFromEthers}>
+          From ethers provider
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={printData}>Print data</button>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
